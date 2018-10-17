4704e25116e6cfa24d33c65462167a2bca17781a
Joshua Suskalo <JZSuskalo@Student.FullSail.edu>
2018-10-17 04:15:22 -0400

    Added filter by date and put default database in the home directory

--- src/notes/core.clj
+++ src/notes/core.clj
@@ -7,11 +7,13 @@
    [clojure.string :as str]
    [clojure.tools.cli :as cli])
   (:import
-   (java.io RandomAccessFile)
+   (java.io File
+            RandomAccessFile)
    (java.nio MappedByteBuffer)
    (java.nio.channels FileChannel$MapMode)
-   (java.time LocalDateTime)
-   (java.util Calendar)
+   (java.time LocalDate
+              LocalTime)
+   (java.time.format DateTimeFormatter)
    (org.iq80.snappy Snappy))
   (:gen-class))
 
@@ -38,7 +40,7 @@
 (s/def ::location int?)
 (s/def ::size pos-int?)
 (s/def ::compressed-size pos-int?)
-(s/def ::metadata (s/keys :req [::location ::size ::compressed-size]
+(s/def ::metadata (s/keys :req [::location ::size ::compressed-size ::date]
                           :opt [::title ::topic ::tags]))
 
 (defn num->month
@@ -61,36 +63,89 @@
                            (partial > 13))))
 
 (defn timecode
-  [& {:keys [hour minute second]}]
-  (let [ret {::hour hour
-             ::minute minute}]
-    (if second
-      (assoc ret ::second second)
-      ret)))
+  ([& {:keys [hour minute second]}]
+   (let [ret {::hour hour
+              ::minute minute}]
+     (if second
+       (assoc ret ::second second)
+       ret))))
 (s/fdef timecode
   :args (s/keys* :req-un [::hour ::minute]
                  :opt-un [::second])
   :ret ::timecode)
 
+(defn timecode-from-java
+  [^LocalTime local-time]
+  (let [hour-of-day (.getHour local-time)
+        minute-of-hour (.getMinute local-time)
+        second-of-minute (.getSecond local-time)]
+    {::hour hour-of-day
+     ::minute minute-of-hour
+     ::second second-of-minute}))
+(s/fdef timecode-from-java
+  :args (s/cat :local-time (partial instance? java.time.LocalTime))
+  :ret ::timecode)
+
+(defn date
+  [& {:keys [month day year timecode]}]
+  (let [ret {::month month
+             ::day day
+             ::year year}]
+    (if timecode
+      (assoc ret ::timecode timecode)
+      ret)))
+(s/fdef date
+  :args (s/keys* :req-un [::month ::day ::year]
+                 :opt-un [::timecode]))
+
+(defn date-from-java
+  ([^LocalDate local-date]
+   (let [day-of-month (.getDayOfMonth local-date)
+         month-of-year (.getMonthValue local-date)
+         year (.getYear local-date)]
+     {::day day-of-month
+      ::month (num->month month-of-year)
+      ::year year}))
+  ([^LocalDate local-date ^LocalTime local-time]
+   (assoc (date-from-java local-date)
+          ::timecode (timecode-from-java local-time))))
+(s/fdef date-from-java
+  :args (s/or :date (s/cat :local-date (partial instance? java.time.LocalDate))
+              :date-time (s/cat :local-date (partial instance? java.time.LocalDate)
+                                :local-time (partial instance? java.time.LocalTime)))
+  :ret ::date)
+
 (defn current-date
   []
-  (let [date-time (LocalDateTime/now)
-        day-of-month (.getDayOfMonth date-time)
-        month-of-year (.getMonthValue date-time)
-        year (.getYear date-time)
-        hour-of-day (.getHour date-time)
-        minute-of-hour (.getMinute date-time)
-        second-of-minute (.getSecond date-time)]
-    {::day day-of-month
-     ::month (num->month month-of-year)
-     ::year year
-     ::timecode (timecode :hour hour-of-day
-                          :minute minute-of-hour
-                          :second second-of-minute)}))
+  (date-from-java (LocalDate/now) (LocalTime/now)))
 (s/fdef current-date
   :args (s/cat)
   :ret ::date)
 
+(defn compare-timecode
+  [t1 t2]
+  (let [v1 [(::hour t1) (::minute t1) (::second t1)]
+        v2 [(::hour t2) (::minute t2) (::second t2)]]
+    (compare v1 v2)))
+(s/fdef compare-timecode
+  :args (s/cat :t1 ::timecode
+               :t2 ::timecode)
+  :ret number?)
+
+(defn compare-date
+  [d1 d2]
+  (let [v1 [(::year d1) (::month d1) (::day d1)]
+        v2 [(::year d2) (::month d2) (::day d2)]
+        res (compare v1 v2)]
+    (if (and (zero? res)
+             (::timecode d1)
+             (::timecode d2))
+      (compare-timecode (::timecode d1) (::timecode d2))
+      res)))
+(s/fdef compare-date
+  :args (s/cat :d1 ::date
+               :d2 ::date)
+  :ret number?)
 
 (defn note
   [& {:keys [date content title topic tags]}]
@@ -154,30 +209,30 @@
        (filter identity)
        (str/join \newline)))
 
-(def ^:dynamic *db-file* "notes.db")
+(def ^{:dynamic true :tag java.io.File} *db-file*
+  (File. (System/getProperty "user.home") ".notes.db"))
 
 (defn append-entry
   [note]
-  (let [file (io/file *db-file*)]
-    (when-not (.exists file)
-      (spit file nil))
-    (with-open [fc (.getChannel (RandomAccessFile. file "rw"))]
-      (let [meta-str (prn-str (select-keys note [::topic ::title ::tags]))
-            meta-arr (Snappy/compress (.getBytes meta-str))
-            note-str (prn-str note)
-            note-arr (Snappy/compress (.getBytes note-str))
-            buffer (.map fc FileChannel$MapMode/READ_WRITE
-                         (.size fc)
-                         (+ (* 5 Long/BYTES)
-                            (count meta-arr)
-                            (count note-arr)))]
-        (.putLong buffer (+ (* 4 Long/BYTES) (count meta-arr) (count note-arr)))
-        (.putLong buffer (count meta-str))
-        (.putLong buffer (count meta-arr))
-        (.put buffer meta-arr)
-        (.putLong buffer (count note-str))
-        (.putLong buffer (count note-arr))
-        (.put buffer note-arr))))
+  (when-not (.exists *db-file*)
+    (spit *db-file* nil))
+  (with-open [fc (.getChannel (RandomAccessFile. *db-file* "rw"))]
+    (let [meta-str (prn-str (select-keys note [::topic ::title ::tags ::date]))
+          meta-arr (Snappy/compress (.getBytes meta-str))
+          note-str (prn-str note)
+          note-arr (Snappy/compress (.getBytes note-str))
+          buffer (.map fc FileChannel$MapMode/READ_WRITE
+                       (.size fc)
+                       (+ (* 5 Long/BYTES)
+                          (count meta-arr)
+                          (count note-arr)))]
+      (.putLong buffer (+ (* 4 Long/BYTES) (count meta-arr) (count note-arr)))
+      (.putLong buffer (count meta-str))
+      (.putLong buffer (count meta-arr))
+      (.put buffer meta-arr)
+      (.putLong buffer (count note-str))
+      (.putLong buffer (count note-arr))
+      (.put buffer note-arr)))
   nil)
 (s/fdef append-entry
   :args (s/cat :note ::note)
@@ -206,53 +261,60 @@
 (defn entry-metadata
   "Returns a lazy sequence of the entries and their titles, tags, and topics."
   []
-  (let [file (io/file *db-file*)]
-    (when-not (.exists file)
-      (spit file nil))
-    (let [fc (.getChannel (RandomAccessFile. file "r"))
-         lazy-fn (fn f [location]
-                   (let [memory-mapped-file (.map fc
-                                                  FileChannel$MapMode/READ_ONLY
-                                                  location
-                                                  (min (* 1024 8)
-                                                       (- (.size fc)
-                                                          location)))]
-                     (lazy-seq (if (<= (.limit memory-mapped-file) 0)
-                                 (do (.close fc)
-                                     nil)
-                                 (let [[tags next-location]
-                                       (read-metadata memory-mapped-file location)]
-                                   (cons tags (f next-location)))))))]
-     (lazy-fn 0))))
+  (when-not (.exists *db-file*)
+    (spit *db-file* nil))
+  (let [fc (.getChannel (RandomAccessFile. *db-file* "r"))
+        lazy-fn (fn f [location]
+                  (let [memory-mapped-file (.map fc
+                                                 FileChannel$MapMode/READ_ONLY
+                                                 location
+                                                 (min (* 1024 8)
+                                                      (- (.size fc)
+                                                         location)))]
+                    (lazy-seq (if (<= (.limit memory-mapped-file) 0)
+                                (do (.close fc)
+                                    nil)
+                                (let [[tags next-location]
+                                      (read-metadata memory-mapped-file location)]
+                                  (cons tags (f next-location)))))))]
+    (lazy-fn 0)))
 (s/fdef entry-tags
   :args (s/cat)
   :ret (s/coll-of ::metadata))
 
 (defn read-entry
   [metadata]
-  (let [file (io/file *db-file*)]
-    (when-not (.exists file)
-      (spit file nil))
-    (with-open [fc (.getChannel (RandomAccessFile. file "r"))]
-      (let [^MappedByteBuffer buffer (.map fc
-                                           FileChannel$MapMode/READ_ONLY
-                                           (::location metadata)
-                                           (::compressed-size metadata))
-            arr ^bytes (make-array Byte/TYPE (::compressed-size metadata))]
-        (.get buffer arr)
-        (edn/read-string (String. (Snappy/uncompress arr 0 (int (::compressed-size metadata)))))))))
+  (when-not (.exists *db-file*)
+    (spit *db-file* nil))
+  (with-open [fc (.getChannel (RandomAccessFile. *db-file* "r"))]
+    (let [^MappedByteBuffer buffer (.map fc
+                                         FileChannel$MapMode/READ_ONLY
+                                         (::location metadata)
+                                         (::compressed-size metadata))
+          arr ^bytes (make-array Byte/TYPE (::compressed-size metadata))]
+      (.get buffer arr)
+      (edn/read-string (String. (Snappy/uncompress arr 0 (int (::compressed-size metadata))))))))
 (s/fdef read-entry
   :args (s/cat :metadata ::metadata)
   :ret ::note)
 
 (defn filter-xf
-  [& {:keys [content title topic tags] :as args}]
+  [& {:keys [content title topic tags before after date] :as args}]
   (let [title-terms (when title
                       (map str/lower-case (str/split title #"\s+")))
         content-terms (when content
                         (map str/lower-case (str/split content #"\s+")))]
     (comp (filter (if topic
                     #(= topic (::topic %))
+                    (constantly true)))
+          (filter (if before
+                    #(>= (compare-date before (::date %)) 0)
+                    (constantly true)))
+          (filter (if after
+                    #(<= (compare-date after (::date %)) 0)
+                    (constantly true)))
+          (filter (if date
+                    #(= (compare-date date (::date %)) 0)
                     (constantly true)))
           (filter (if tags
                     #(empty? (st/difference tags (::tags %)))
@@ -291,11 +353,21 @@
    ["-t" "--tags \"[TAG*]\"" "Sets the tags on an add, and requires tags on a search"
     :parse-fn #(let [s (edn/read-string %)]
                  (set (map keyword s)))]
-   ["-f" "--file FILE" "Specifies a file to be used, useful when you have multiples"]
-   ["-r" "--results NUM-RESULTS" "Specifies how many results should be given in a search"
+   ["-f" "--file FILE" "Specifies a file to be used, useful when you have multiples (default ~/.notes.db)"]
+   ["-n" "--num-results NUM-RESULTS" "Specifies how many results should be given in a search, either a number, or \"all\" (default 1)"
     :parse-fn #(if (= % "all")
                  %
-                 (Long/parseLong %))]])
+                 (Long/parseLong %))]
+   ["-r" "--reverse" "Include this flag in a search if you want to reverse the order of results"]
+   ["-a" "--after MM-DD-YYYY" "Search only for notes after this date"
+    :parse-fn #(let [date-format (DateTimeFormatter/ofPattern "MM-dd-uuuu")]
+                 (date-from-java (LocalDate/parse % date-format) (LocalTime/of 0 0)))]
+   ["-b" "--before MM-DD-YYYY" "Search only for notes before this date"
+    :parse-fn #(let [date-format (DateTimeFormatter/ofPattern "MM-dd-uuuu")]
+                 (date-from-java (LocalDate/parse % date-format) (LocalTime/of 23 59)))]
+   ["-d" "--date MM-DD-YYYY" "Add the note to this date, or search for a note on this exact date"
+    :parse-fn #(let [date-format (DateTimeFormatter/ofPattern "MM-dd-uuuu")]
+                 (date-from-java (LocalDate/parse % date-format)))]])
 
 (defn apply-keyword-args
   [f & args]
@@ -308,27 +380,35 @@
     (if (:help options)
       (when summary
         (println (usage summary)))
-      (binding [*db-file* (or (:file options)
-                              *db-file*)]
-        (if (seq arguments)
-          (case (str/lower-case (first arguments))
-            "add" (append-entry (note :content (rest arguments)
-                                      :title (:title options)
-                                      :topic (:topic options)
-                                      :tags (:tags options)
-                                      :date (current-date)))
-            "search" (let [entries (sequence (apply-keyword-args
-                                              filter-xf
-                                              :content (str/join \space (rest arguments))
-                                              options)
-                                             (entry-metadata))
-                           entries (if (= (:results options) "all")
-                                     entries
-                                     (take (or (:results options)
-                                               1)
-                                           entries))]
-                       (println (str "Searching for " (or (:results options) 1) " entries in your notes...\n"))
-                       (doseq [entry (interpose "\n" entries)]
-                         (println entry)))
-            (println "Invalid command. Try \"notes --help\" to see how the program is used"))
-          (println "Invalid command. Try \"notes --help\" to see how the program is used"))))))
+      (if errors
+        (println "Invalid command. Try \"notes --help\" to see how the program is used")
+        (binding [*db-file* (or (when (:file options)
+                                  (File. ^String (:file options)))
+                                *db-file*)]
+          (if (seq arguments)
+            (case (str/lower-case (first arguments))
+              "add" (do (println "Adding a new ")
+                        (append-entry (note :content (rest arguments)
+                                            :title (:title options)
+                                            :topic (:topic options)
+                                            :tags (:tags options)
+                                            :date (or (:date options)
+                                                      (current-date)))))
+              "search" (let [entries (sequence (apply-keyword-args
+                                                filter-xf
+                                                :content (str/join \space (rest arguments))
+                                                options)
+                                               (entry-metadata))
+                             entries (if (:reverse options)
+                                       (reverse entries)
+                                       entries)
+                             entries (if (= (:results options) "all")
+                                       entries
+                                       (take (or (:results options)
+                                                 1)
+                                             entries))]
+                         (println (str "Searching for " (or (:results options) 1) " entries in your notes...\n"))
+                         (doseq [entry (interpose "\n" entries)]
+                           (println entry)))
+              (println "Invalid command. Try \"notes --help\" to see how the program is used"))
+            (println "Invalid command. Try \"notes --help\" to see how the program is used")))))))
