4f1690abe7f522eb5f5044105a45641d3918c95f
Joshua Suskalo <joshua@suskalo.org>
2023-06-30 12:03:18 -0500

    Add basic nix shell

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
