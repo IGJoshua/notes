5a3771b7cbe5ab2923f7f1169ef795fb537a9cf1
Joshua Suskalo <joshua@suskalo.org>
2023-06-30 20:03:04 -0500

    Add clojure builder to flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    clj-nix = {
+      url = "github:jlesquembre/clj-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, clj-nix }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          cljpkgs = clj-nix.packages.${system};
+        in
         {
           devShells.default = import ./shell.nix { inherit pkgs; };
+          packages = rec {
+            cljnotes-clj = cljpkgs.mkCljBin {
+              projectSrc = ./.;
+              name = "org.suskalo/cljnotes";
+              main-ns = "notes.core";
+              jdkRunner = pkgs.jdk17_headless;
+              buildCommand = "clj -T:build uber";
+            };
+            cljnotes-jdk = cljpkgs.customJdk {
+              cljDrv = self.packages.${system}.cljnotes-clj;
+              locales = "en";
+            };
+            default = cljnotes-jdk;
+          };
+          apps.default = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/cljnotes";
+          };
         }
       );
 }
